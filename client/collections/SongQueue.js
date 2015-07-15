f96b9f21571c4636b5a8cee88c1d43118bcabaae
Hack Reactor Students <accounts@hackreactor.com>
2015-07-15 15:21:35 -0700

    Implement passing solution for enqueing songs.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,13 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function() {
+  },
+  enqueue: function(song) {
+    this.push(song);
+  },
+  dequeue: function() {
+    return this.shift();
   }
 
 });
